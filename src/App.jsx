b3c54e59411098d06142f4024f826555c405b818
PC1 <kevsalomdev@gmail.com>
2023-09-29 12:17:22 -0400

    NavBar y Footer en Home, Colocacion de Home como Vista

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
-import Home from './components/Home/Home'
+import Home from './Views/Home/Home.jsx'
+
+
 function App() { 
  
   return (
---
+++ src/Views/Home/Home.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar"
+import Footer from "../../components/Footer/Footer"
+
+
+const Home = () => {
+    
+    return (
+        <div>
+            <NavBar/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home;
+
+
+
+// const { getSales } = useStore() //esto actua como nuestro dispatch, guardamos en la variable la action getSales
+//     const { sales } = useStore.getState(); // obtenemos del estado global la variable sales(productos en oferta)
+
+//     //hooks
+//     useEffect(() => {
+//         getSales() //al montar componente ejecutamos la action q modifica nuestro estado global
+//     }, [])
+
+//     return (
+//         <div>
+//             <h1 className='card-title'>Sales</h1>
+//             <Carousel>
+//                 {
+//                     sales.map((product) => (
+//                         <Carousel.Item key={product.id}>
+//                             <CardProduct  // llamamos el componente Card pasandole como props los datos de la variable global                        
+//                                 name={product.name}
+//                                 description={product.description}
+//                             />
+//                         </Carousel.Item>
+//                     ))}
+
+//             </Carousel>
+//         </div>
+//     )
+
+
