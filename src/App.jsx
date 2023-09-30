40ef7541ced142eb9aa990ad302c4677f5c33979
Hernan Parino <you@example.com>
2023-09-30 09:09:13 -0300

    Guardando cambios locales en src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { Route, Routes } from 'react-router'
-import './App.css'
-import Home from './Views/Home/Home.jsx'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Home from "./Views/Home/Home.jsx";
+import Legal from "./Views/Legal/Legal";
 
-
-function App() { 
- 
+function App() {
   return (
-    <>      
+    <>
       <Routes>
-        <Route path='/' element={<Home/>}/>        
-      </Routes>       
+        <Route path="/" element={<Home />} />
+        <Route path="/legal/" element={<Legal />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
