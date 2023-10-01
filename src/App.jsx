4b7c0bfc6abcc3e1cd6610141c299db4dd0c5922
PC1 <kevsalomdev@gmail.com>
2023-09-30 21:03:24 -0400

    adding error404 and legal in home

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import { Route, Routes } from 'react-router'
-import './App.css'
-import Category from './Views/Category/Category'
-import ThemeView from './Views/ThemeView/ThemeView'
-import Home from './Views/Home/Home.jsx'
-import ProductDetails from './Views/ProductDetails/ProductDetails'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Home from "./Views/Home/Home.jsx";
+import ProductDetails from "./Views/ProductDetails/ProductDetails";
+import Legal from "./Views/Legal/Legal";
+import Error404 from "./Views/Error404/Error404";
 import NavBar from "./components/NavBar/NavBar"
 import Footer from "./components/Footer/Footer"
+import ThemeView from './Views/ThemeView/ThemeView'
+import Category from './Views/Category/Category'
 
-
-function App() { 
- 
+function App() {
   return (
-    <div className='vh-100'>         
-     <NavBar/>
-            
+    <div className="vh-100 d-flex flex-column">
+      <NavBar />
+      <div className="flex-grow-1">
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/category/:nameCategory' element={<Category/>}/>
-        <Route path='/thematic/:nameThematic' element={<ThemeView/>}/>
-        <Route path='/product/:id' element={<ProductDetails/>}/>
-      </Routes>  
-
-      <Footer/>     
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:nameCategory" element={<Category />} />
+        <Route path="/thematic/:nameThematic" element={<ThemeView />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/legal" element={<Legal />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
