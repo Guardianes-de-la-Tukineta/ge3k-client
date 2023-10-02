dbf1cfa4904d0fe809af4615ac46f4a1349b4ff9
Cristian Guzman <mono-717@hotmail.com>
2023-10-02 21:35:59 +0200

    paginado condicional y efecto hover tarjetas

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { useStore } from "../../zustand/useStore/useStore";
 import CardProduct from "../CardProduct/CardProduct";
 import PaginationCards from "../Pagination/PaginationCards";
@@ -6,20 +6,20 @@ import style from './CardContainer.module.css'
 
 const CardContainer = () => {
     //estados 
-    const[cardsPerPagin,setCardsPerPagin]=useState(12); //numero de targetas por pag.para mostrar
-    const[currentPage,setCurrentPage] =useState(1) // pag. actual
+    const [cardsPerPagin, setCardsPerPagin] = useState(12); //numero de targetas por pag.para mostrar
+    const [currentPage, setCurrentPage] = useState(1) // pag. actual
 
-    const { currentProducts } = useStore((state)=>({ //nos traemos la variable global
-        currentProducts:state.currentProducts
+    const { currentProducts } = useStore((state) => ({ //nos traemos la variable global
+        currentProducts: state.currentProducts
     }))
-    const totalProducts=currentProducts.length // para saber el total de productos q vienen del back
+    const totalProducts = currentProducts.length // para saber el total de productos q vienen del back
 
-    const showProducts=currentProducts.slice(currentPage*cardsPerPagin-cardsPerPagin, currentPage*cardsPerPagin) //logica para rebanar el array total, y mostrar solo las cardsperpag correspondientes
-    
+    const showProducts = currentProducts.slice(currentPage * cardsPerPagin - cardsPerPagin, currentPage * cardsPerPagin) //logica para rebanar el array total, y mostrar solo las cardsperpag correspondientes
+
     return (
-        <div className="row">       
+        <div className="row">
             {
-                showProducts.map((product)=>(
+                showProducts.map((product) => (
                     <CardProduct
                         name={product.name}
                         key={product.id}
@@ -29,17 +29,20 @@ const CardContainer = () => {
                 ))
             }
             <div className='d-flex justify-content-center'>
-                <PaginationCards 
-                    cardsPerPagin={cardsPerPagin} 
-                    currentPage={currentPage} 
-                    setCurrentPage={setCurrentPage} 
-                    totalProducts={totalProducts}
-                /> 
+                {
+                    totalProducts > cardsPerPagin &&
+                    <PaginationCards
+                        cardsPerPagin={cardsPerPagin}
+                        currentPage={currentPage}
+                        setCurrentPage={setCurrentPage}
+                        totalProducts={totalProducts}
+                    />
+                }
             </div>
         </div>
-          
+
     )
-    
+
 }
 
 export default CardContainer;
