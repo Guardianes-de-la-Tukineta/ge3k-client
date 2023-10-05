6ad9ffb3b23a2154be46d57b5ffd587a166eea87
PC1 <kevsalomdev@gmail.com>
2023-10-04 23:36:54 -0400

    ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
-
-import CardContainer from "./components/CardContainer/CardContainer";
 import Home from "./Views/Home/Home";
-
-
-// import Legal from "./Views/Legal/Legal";
-
-
-
 import ProductDetails from "./Views/ProductDetails/ProductDetails";
 import Legal from "./Views/Legal/Legal";
 import Error404 from "./Views/Error404/Error404";
-
-
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import ThemeView from "./Views/ThemeView/ThemeView";
 import Category from "./Views/Category/Category";
-
-
 import SearchResults from "./Views/SearchResults/SearchResults";
 import Login from "./components/Login/Login";
 import Logout from "./components/Login/Logout";
 import Profile from "./components/Profile/Profile";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop"; //Para poder ir al inicio (arriba) de la pagina al cambiar de vista
 
 function App() {
   return (
     <div className="vh-100 d-flex flex-column">
       <NavBar />
+      <ScrollToTop/> 
       <div className="flex-grow-1">
         <Routes>
           <Route path="/" element={<Home />} />
