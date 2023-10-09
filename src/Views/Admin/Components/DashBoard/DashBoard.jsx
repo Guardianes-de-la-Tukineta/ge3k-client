f993061143c8a800a9a7cde7740c02a8522cfa79
PC1 <kevsalomdev@gmail.com>
2023-10-08 20:51:38 -0400

    All DashBoardCards Done

--- src/Views/Admin/Components/DashBoard/DashBoard.jsx
+++ src/Views/Admin/Components/DashBoard/DashBoard.jsx
@@ -3,7 +3,7 @@ import style from './DashBoard.module.css'
 
 const DashBoard = () => {
   return (
-    <div className='container'>
+    <div className='container d-flex flex-column flex-grow-1 justify-content-around' style={{paddingBottom:'1rem'}} >
         <div className='row'>
             <div className='col-md-4 '>
               <div className={style.cardInfoContainer}>
@@ -14,14 +14,14 @@ const DashBoard = () => {
             </div>
             <div className='col-md-4 '>
               <div className={style.cardInfoContainer}>
-                <span><i className="bi bi-credit-card"></i></span>
+                <span><i class="bi bi-credit-card-fill"></i></span>
                 <span className={style.number}>150</span>
                 <span>Total Sales</span>
               </div>
             </div>
             <div className='col-md-4 '>
               <div className={style.cardInfoContainer}>
-                <span><i className="bi bi-person-check"></i></span>
+                <span><i class="bi bi-person-fill-check"></i></span>
                 <span className={style.number}>72</span>
                 <span>Registered Users</span>
               </div>
@@ -37,12 +37,12 @@ const DashBoard = () => {
                 <div className={`${style.cardCreateProduct} ` }  style={{margin:'1.5rem 2.5rem',}}>
               <div className={`container-fluid  `}> 
                 <div className={`row`}>
-                 <div className= {`col-9 ${style.itemCreateProduct} p-4`} >
+                 <div className= {`col-9 ${style.itemCreateProduct}`} >
             
-                 <i className="bi bi-bag-plus"></i>
-                 <span>Create New Product</span>
+                 <i class="bi bi-bag-plus-fill"></i>
+                 <span>CREATE NEW PRODUCT</span>
                  </div>
-                 <div className={`col-3 ${style.arrowContainer} p-4`}>
+                 <div className={`col-3 ${style.arrowContainer}`}>
                  <i class="bi bi-arrow-right"></i>
               </div>
 
@@ -60,10 +60,10 @@ const DashBoard = () => {
                 <div className={`${style.cardCreateProduct} ` }  style={{margin:'1.5rem 2.5rem',}}>
               <div className={`container-fluid  `}> 
                 <div className={`row`}>
-                 <div className= {`col-9 ${style.itemCreateProduct} p-4`} >
+                 <div className= {`col-9 ${style.itemCreateProduct}`} >
             
-                 <i className="bi bi-bag-check"></i>
-                 <span>Edit Product</span>
+                 <i class="bi bi-bag-dash-fill"></i>
+                 <span>EDIT PRODUCT</span>
                  </div>
                  <div className={`col-3 ${style.arrowContainer} p-4`}>
                  <i class="bi bi-arrow-right"></i>
@@ -84,10 +84,10 @@ const DashBoard = () => {
                 <div className={`${style.cardCreateProduct} ` }  style={{margin:'1.5rem 2.5rem',}}>
               <div className={`container-fluid  `}> 
                 <div className={`row`}>
-                 <div className= {`col-9 ${style.itemCreateProduct} p-4`} >
+                 <div className= {`col-9 ${style.itemCreateProduct}`} >
             
-             <i className="bi bi-bag-dash"></i>
-             <span>Delete Product</span>
+                 <i class="bi bi-bag-x-fill"></i>
+             <span>DELETE PRODUCT</span>
                  </div>
                  <div className={`col-3 ${style.arrowContainer} p-4`}>
                  <i class="bi bi-arrow-right"></i>
@@ -99,6 +99,73 @@ const DashBoard = () => {
                 </div>
               </div>
         </div>
+
+
+        <div className='row'>
+           
+            <div className={`col-md-4 `} style={{padding:'0 '}}>
+                <div className={`${style.cardForUsers} ` }  style={{margin:'1.5rem 2.5rem',}}>
+              <div className={`container-fluid  `}> 
+                <div className={`row`}>
+                 <div className= {`col-9 ${style.itemCreateProduct}`} >
+            
+                 <i class="bi bi-person-fill-gear"></i>
+                 <span>MANAGE USERS</span>
+                 </div>
+                 <div className={`col-3 ${style.arrowContainer} p-4`}>
+                 <i class="bi bi-arrow-right"></i>
+              </div>
+
+
+                </div>
+                </div>
+                </div>
+              </div>
+
+
+
+
+            <div className={`col-md-4 `} style={{padding:'0 '}}>
+                <div className={`${style.cardForUsers} ` }  style={{margin:'1.5rem 2.5rem',}}>
+              <div className={`container-fluid  `}> 
+                <div className={`row`}>
+                 <div className= {`col-9 ${style.itemCreateProduct}`} >
+            
+                 <i class="bi bi-person-fill-lock"></i>
+             <span>MANAGE ADMINISTRATORS</span>
+                 </div>
+                 <div className={`col-3 ${style.arrowContainer} p-4`}>
+                 <i class="bi bi-arrow-right"></i>
+              </div>
+
+
+                </div>
+                </div>
+                </div>
+              </div>
+
+              <div className={`col-md-4 `} style={{padding:'0 '}}>
+                <div className={`${style.cardForUsers} ` }  style={{margin:'1.5rem 2.5rem',}}>
+              <div className={`container-fluid  `}> 
+                <div className={`row`}>
+                 <div className= {`col-9 ${style.itemCreateProduct}`} >
+            
+                 <i class="bi bi-person-plus-fill"></i>
+             <span>ADD NEW ADMINISTRATOR</span>
+                 </div>
+                 <div className={`col-3 ${style.arrowContainer} p-4`}>
+                 <i class="bi bi-arrow-right"></i>
+              </div>
+
+
+                </div>
+                </div>
+                </div>
+              </div>
+
+        </div>
+
+
     </div>
   )
 }
