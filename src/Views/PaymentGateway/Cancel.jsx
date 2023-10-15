06059e341e06aa9ddf6204b39287bfa6e74e5051
Leonardo Villarraga <leonardo381@gmail.com>
2023-10-15 17:04:34 -0500

    LeoStripe

--- src/Views/PaymentGateway/Cancel.jsx
+++ src/Views/PaymentGateway/Cancel.jsx
@@ -1,11 +1,22 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom"; // Si deseas agregar un enlace a otra página
+
+const PaymentSuccessView = () => {
+  return (
+    <div  className="text-center my-5">
+      <h1>¡The purchase has been canceled!</h1>
+      <p className="my-5">Please try again your purchase if was  an error</p>
+      
+      {/* Opcional: Agregar un enlace a otra página */}
+      <Link to="/">
+        <Button style={{borderRadius:'10px', padding:'10px 5px', margin:'0 3px', backgroundColor:'#ff6824', border:'none', fontWeight:'600'}} >Volver a la página de inicio</Button>
+      </Link>
+
+    </div>
+  );
+};
+
+export default PaymentSuccessView;
 
 
-const Cancel = () => {
-
-    return(
-        <h1>Sorry to see you cancelled your payment </h1>
-    )
-
-}
-
-export default Cancel;
--- src/Views/PaymentGateway/Success.jsx
+++ src/Views/PaymentGateway/Success.jsx
@@ -1,10 +1,46 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom"; // Si deseas agregar un enlace a otra página
 
-const Success = () => {
+const PaymentSuccessView = () => {
+  return (
+    <div  className="text-center my-5">
+      <h1>¡The purchase has been successful!</h1>
+      <p className="my-5">Thank you for your purchase and we hope to have the pleasure of serving you again.</p>
+      
+      {/* Opcional: Agregar un enlace a otra página */}
+      <Link to="/">
+        <Button style={{borderRadius:'10px', padding:'10px 5px', margin:'0 3px', backgroundColor:'#ff6824', border:'none', fontWeight:'600'}}>Volver a la página de inicio</Button>
+      </Link>
 
-    return(
-        <h1> Thank you for your purchase! </h1>
-    )
+      {/* Opcional: Agregar un botón para imprimir el comprobante */}
+      <Button style={{borderRadius:'10px', padding:'10px 5px', margin:'0 3px', backgroundColor:'#ff6824', border:'none', fontWeight:'600'}}  className="ml-3" onClick={() => window.print()}>
+        Imprimir comprobante
+      </Button>
+    </div>
+  );
+};
 
-}
+export default PaymentSuccessView;
 
-export default Success;
+
+
+
+
+
+
+
+
+
+
+
+
+// const Success = () => {
+
+//     return(
+//         <h1> Thank you for your purchase! </h1>
+//     )
+
+// }
+
+// export default Success;
