234d67269744127988a65ba9af1e705c94d9089a
PC1 <kevsalomdev@gmail.com>
2023-10-19 19:36:57 -0400

    confirmation eyes for AddAdminForm

--- src/Views/Admin/Components/FormAddAdmin/FormAddAdmin.jsx
+++ src/Views/Admin/Components/FormAddAdmin/FormAddAdmin.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./FormAddAdmin.module.css"
 import { useForm } from "react-hook-form";
 import Spinner from "react-bootstrap/Spinner";
@@ -6,6 +6,18 @@ import Spinner from "react-bootstrap/Spinner";
 
 
 const FormAddAdmin = ({handleCreateNewAdmin, loading}) => {
+
+
+const [showPassword, setShowPassword] = useState(false);
+const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+  setShowPassword(!showPassword);
+} ;
+
+const toggleConfirmPasswordVisibility = () => {
+  setShowConfirmPassword(!showConfirmPassword);
+} ;
 
   const {
     register,
@@ -34,7 +46,6 @@ const FormAddAdmin = ({handleCreateNewAdmin, loading}) => {
 
   return (
     <div className={`container-fluid`} style={{ padding: "1rem 2.4rem" }}>
- 
       <div className={`row ${style.rowContainer}`}>
         <form
           onSubmit={handleSubmit(onSubmit)}
@@ -49,7 +60,6 @@ const FormAddAdmin = ({handleCreateNewAdmin, loading}) => {
               onChange={(e) => {
                 setValue("name", e.target.value);
                 trigger("name");
-      
               }}
             />
             {errors.name && (
@@ -65,7 +75,6 @@ const FormAddAdmin = ({handleCreateNewAdmin, loading}) => {
               onChange={(e) => {
                 setValue("surname", e.target.value);
                 trigger("surname");
-      
               }}
             />
             {errors.surname && (
@@ -75,68 +84,105 @@ const FormAddAdmin = ({handleCreateNewAdmin, loading}) => {
 
           <label>
             <span>Email:</span>
-            <input 
-
-        id='email'
-        placeholder="Email" 
-        {...register("email", { 
-          required: "This field is required.",
-          pattern: {
-            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-            message: "Invalid email address"
-          }
-        })} 
-        onChange={(e) => {
-          setValue("email", e.target.value);
-          trigger("email");
-
-        }}
-      />
-      {errors.email && <span className={style.error}>{errors.email.message}</span>}
+            <input
+              id="email"
+              placeholder="Email"
+              {...register("email", {
+                required: "This field is required.",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: "Invalid email address",
+                },
+              })}
+              onChange={(e) => {
+                setValue("email", e.target.value);
+                trigger("email");
+              }}
+            />
+            {errors.email && (
+              <span className={style.error}>{errors.email.message}</span>
+            )}
           </label>
 
           <label>
             <span>Password:</span>
-            <input 
-               type="password" 
-               placeholder="Create a password"
-        {...register('password', {
-          required: 'This field is required',
-          pattern: {
-            value: /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%.*?&]{10,}$/,
-            message: 'Use 1 uppercase, 1 number, 1 symbol, and a minimum of 10 characters'
-          }
-        })}
-        onChange={(e) => {
-          setValue("password", e.target.value);
-          trigger("password");
+            <div className={style.passwordInput}>
+            <input
+        
+              type={(!showPassword)? "password" : "text"} 
+              placeholder="Create a password"
+              {...register("password", {
+                required: "This field is required",
+                pattern: {
+                  value:
+                    /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%.*?&]{10,}$/,
+                  message:
+                    "Use 1 uppercase, 1 number, 1 symbol, and a minimum of 10 characters",
+                },
+              })}
+              onChange={(e) => {
+                setValue("password", e.target.value);
+                trigger("password");
+              }}
+            />
 
-        }}
-      />
-      {errors.password && <span className={style.error}>{errors.password.message}</span>}
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className={style.showPasswordButton}
+            >
+              {!showPassword ? (
+                 <i className="bi bi-eye-slash-fill"></i>
+           
+              ) : (
+                <i className="bi bi-eye-fill"></i>
+              )}
+            </button>
+           </div>
+            {errors.password && (
+              <span className={style.error}>{errors.password.message}</span>
+            )}
           </label>
 
           <label>
+         
             <span>Confirm password:</span>
-            <input 
-               type="password" 
-               placeholder="Confirm the password"
-        {...register('confirmPassword', {
-          required: 'This field is required',
-          validate: value =>
-            value === getValues().password || "The passwords do not match"
-        })}
-        onChange={(e) => {
-          setValue("confirmPassword", e.target.value);
-          trigger("confirmPassword");
-
-        }}
-      />
-      {errors.confirmPassword && <span className={style.error}>{errors.confirmPassword.message}</span>}
+            <div className={style.passwordInput}>
+            <input
+              type={(!showConfirmPassword)?"password":"text"}  
+              placeholder="Confirm the password"
+              {...register("confirmPassword", {
+                required: "This field is required",
+                validate: (value) =>
+                  value === getValues().password ||
+                  "The passwords do not match",
+              })}
+              onChange={(e) => {
+                setValue("confirmPassword", e.target.value);
+                trigger("confirmPassword");
+              }}
+            />
+             <button
+              type="button"
+              onClick={toggleConfirmPasswordVisibility}
+              className={style.showPasswordButton}
+            >
+              {!showConfirmPassword ? (
+                 <i className="bi bi-eye-slash-fill"></i>
+           
+              ) : (
+                <i className="bi bi-eye-fill"></i>
+              )}
+            </button>
+           </div>
+            {errors.confirmPassword && (
+              <span className={style.error}>
+                {errors.confirmPassword.message}
+              </span>
+            )}
           </label>
 
-
-          <button disabled={!isValid}>
+          <button type="submit" disabled={!isValid}>
             {!loading ? (
               "Create New Admin"
             ) : (
