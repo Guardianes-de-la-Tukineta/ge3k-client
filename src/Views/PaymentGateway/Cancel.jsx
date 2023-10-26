32e978cef8926c40c4a918d17724d9baa893f392
Leonardo Villarraga <leonardo381@gmail.com>
2023-10-26 15:15:23 -0500

    LeoCompCancel

--- src/Views/PaymentGateway/Cancel.jsx
+++ src/Views/PaymentGateway/Cancel.jsx
@@ -3,7 +3,6 @@ import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom"; // Si deseas agregar un enlace a otra página
 import { useEffect } from "react";
 import Swal from 'sweetalert2';
-import cart from "../../Images/cart.jpg";
 
 
 const PaymentSuccessView = () => {
