ad6cf1fdbff9c0ccbc46888d0634a8d75bd8eae4
Leonardo Villarraga <leonardo381@gmail.com>
2023-10-26 11:58:03 -0500

    LeoBranch

--- src/Views/PaymentGateway/Cancel.jsx
+++ src/Views/PaymentGateway/Cancel.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Button, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom"; // Si deseas agregar un enlace a otra página
-import { useState } from "react";
 import { useEffect } from "react";
 import cartCanceled from "../../Images/cartCanceled.jpg";
 import Swal from 'sweetalert2';
@@ -9,22 +8,16 @@ import Swal from 'sweetalert2';
 
 const PaymentSuccessView = () => {
 
-  const [ show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   useEffect(()=> {
-    //shows modal after 1 second
+    //shows after .6 secs
     const timer = setTimeout(()=> {
-      // handleShow();
+     
       Swal.fire({
         title: 'Failed To Purchase',
         text: 'The purchase was not successful, if an error has occurred please try again',
         imageUrl: cartCanceled, 
         imageWidth: 150, 
         imageHeight: 150, 
-        // icon: 'warning',
         confirmButtonText: 'OK',
         confirmButtonColor: '#ff6824',
       });
@@ -40,7 +33,6 @@ const PaymentSuccessView = () => {
       <h1>¡The purchase has been canceled!</h1>
       <p className="my-5">Please try again your purchase if was  an error</p>
       
-      {/* Opcional: Agregar un enlace a otra página */}
       <Link to="/">
         <Button style={{borderRadius:'10px', padding:'10px 5px', margin:'0 3px', backgroundColor:'#ff6824', border:'none', fontWeight:'600'}} >Volver a la página de inicio</Button>
       </Link>
